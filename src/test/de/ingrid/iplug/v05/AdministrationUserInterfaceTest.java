d1500a626dd85c2434f342bcce0c6e83e04cf9ba
hs <hs@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-07-26 14:48:22 +0000

    initial commit

---
+++ src/test/de/ingrid/iplug/v05/AdministrationUserInterfaceTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 1997-2005 by media style GmbH
+ * 
+ * $Source: DispatcherTest.java,v $
+ */
+package de.ingrid.iplug.v05;
+
+import junit.framework.TestCase;
+
+/**
+ * Test for administration-userinterface-feature (INGRID-13).
+ * created on 21.07.2005 <p>
+ *
+ * @author hs
+ */
+public class AdministrationUserInterfaceTest extends TestCase{
+	
+	public void testWebInterface() {
+        fail("INGRID-56 not yet implemented"); 
+    }
+	
+	public void testSetupOfCarrier() {
+        fail("INGRID-58 not yet implemented"); 
+    }
+	
+	public void testSetupOfParentIBus() {
+        fail("INGRID-62 not yet implemented"); 
+    }
+	
+	public void testSetupOfP2PGroup() {
+        fail("INGRID-63 not yet implemented");
+    }
+	
+	public void testSetupOfDatabaseConnection() {
+        fail("INGRID-64 not yet implemented");
+    }
+
+	public void testSetupOfDataTypes() {
+        fail("INGRID-65 not yet implemented");
+    }
+	
+	public void testSetupOfPathToIndex() {
+        fail("INGRID-66 not yet implemented");
+    }
+	
+	public void testSetupScheduledReindexing() {
+        fail("INGRID-67 not yet implemented");
+    }
+	
+	public void testManualReindexing() {
+        fail("INGRID-68 not yet implemented");
+    }
+	
+	public void testSetupOfDatafieldMapping() {
+        fail("INGRID-69 not yet implemented");
+    }
+	
+	public void testSetupLocalRanking() {
+        fail("INGRID-71 not yet implemented");
+    }
+}

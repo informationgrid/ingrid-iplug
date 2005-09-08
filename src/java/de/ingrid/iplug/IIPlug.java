f691365d390f76fe69ee487b83e2ea1d4aadf937
sg <sg@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-09-08 10:42:43 +0000

    add getDataType methode definition.

--- src/java/de/ingrid/iplug/IIPlug.java
+++ src/java/de/ingrid/iplug/IIPlug.java
@@ -10,21 +10,30 @@ package de.ingrid.iplug;
  * Container for meta data that describes a localy or remote living IPlug
  * 
  * created on 09.08.2005
- * @author  sg
+ * 
+ * @author sg
  * @version $Revision: 1.3 $
  */
 public interface IIPlug {
 
     /**
      * A network wide unique id for the IIPlug
-     * @return The unique IPlug identifier 
+     * 
+     * @return The unique IPlug identifier
      */
     public String getId();
-    
+
     /**
      * Allows getter access to the supported datafields
+     * 
      * @return The supported data fields
      */
     public String[] getFields();
+
+    /**
+     * @return the data type of this plug or <code>null</code> if no data type
+     *         was defined
+     */
+    public String getDataType();
 
 }

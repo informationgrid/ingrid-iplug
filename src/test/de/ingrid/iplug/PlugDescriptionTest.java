fbd156dbe88dc9439f7bf9d26c2ee1455ee1b262
sg <sg@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-02-12 14:41:34 +0000

    extend plugDescription test

--- src/test/de/ingrid/iplug/PlugDescriptionTest.java
+++ src/test/de/ingrid/iplug/PlugDescriptionTest.java
@@ -26,7 +26,27 @@ public class PlugDescriptionTest extends TestCase {
         PlugDescription description2 = (PlugDescription) serializer.deSerialize(target);
 
         assertEquals(description1, description2);
+        target.delete();
 
+    }
+    
+    public void testDataType() throws Exception {
+        File target = new File("./testFile.xml");
+        PlugDescription description = new PlugDescription();
+        description.addDataType("A");
+        description.addDataType("B");
+        assertEquals(2, description.getDataTypes().length);
+        assertEquals("A", description.getDataTypes()[0]);
+        assertEquals("B", description.getDataTypes()[1]);
+        XMLSerializer serializer = new XMLSerializer();
+        serializer.aliasClass(PlugDescription.class.getName(),
+                PlugDescription.class);
+        serializer.serialize(description, target);
+
+        PlugDescription description2 = (PlugDescription) serializer.deSerialize(target);
+        assertEquals(2, description2.getDataTypes().length);
+        assertEquals("A", description2.getDataTypes()[0]);
+        assertEquals("B", description2.getDataTypes()[1]);
     }
 
 }

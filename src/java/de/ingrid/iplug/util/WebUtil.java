f2833e6a6b7cd95dcf9560dcd8aad9204565e471
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-02-01 09:48:46 +0000

    add method to get parameters

--- src/java/de/ingrid/iplug/util/WebUtil.java
+++ src/java/de/ingrid/iplug/util/WebUtil.java
@@ -7,21 +7,33 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class WebUtil {
 
-	/**
-     * Returns a parameter from a HttpServletRequest by its key. It is possible to set a default value
-     * in the case the parameter doesn't exist. 
-	 * @param request The servlet request.
-	 * @param key The parameter name.
-	 * @param defaultValue The default value to set.
-	 * @return The value of a parameter identified by its name.
-	 */
-	public static String getParameter(HttpServletRequest request, String key,
-			String defaultValue) {
-		String parameter = request.getParameter(key);
-		if (parameter != null) {
-			return parameter;
-		}
-		return defaultValue;
-	}
+    /**
+     * Returns a parameter from a HttpServletRequest by its key. It is possible
+     * to set a default value in the case the parameter doesn't exist.
+     * 
+     * @param request
+     *            The servlet request.
+     * @param key
+     *            The parameter name.
+     * @param defaultValue
+     *            The default value to set.
+     * @return The value of a parameter identified by its name.
+     */
+    public static String getParameter(HttpServletRequest request, String key, String defaultValue) {
+        String parameter = request.getParameter(key);
+        if (parameter != null) {
+            return parameter;
+        }
+        return defaultValue;
+    }
+
+    public static String[] getParameters(HttpServletRequest request, String parameterName, String[] defaultValues) {
+        String[] parameter = request.getParameterValues(parameterName);
+        String[] returnValue = defaultValues;
+        if (parameter != null && parameter.length > 0) {
+            returnValue = parameter;
+        }
+        return returnValue;
+    }
 
 }

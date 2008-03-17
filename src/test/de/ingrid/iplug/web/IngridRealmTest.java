1e4cb60da4969be65c15671b2d4eda0e332c5283
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2008-03-17 16:24:53 +0000

    remove import

--- src/test/de/ingrid/iplug/web/IngridRealmTest.java
+++ src/test/de/ingrid/iplug/web/IngridRealmTest.java
@@ -3,27 +3,23 @@
  */
 package de.ingrid.iplug.web;
 
+import java.io.FileInputStream;
 import java.security.Principal;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
+
 
 import junit.framework.TestCase;
+import net.weta.components.communication.ICommunication;
+import net.weta.components.communication.tcp.StartCommunication;
 import de.ingrid.ibus.client.BusClient;
+import de.ingrid.iplug.web.IngridRealm.User;
 
 /**
  * 
  */
 public class IngridRealmTest extends TestCase {
 
-    private IngridRealm fIRealm;
 
-    protected void setUp() throws Exception {
-//        BusClient client = BusClient.instance();
-//        client.setBusUrl("/torwald-ibus:ibus-torwald");
-//        client.setJxtaConfigurationPath("/jxta.properties");
-//        this.fIRealm = new IngridRealm("IngridRealm", client.getBus(), "SHA-1");
-    }
     
     public void testTest() throws Exception {
         assertTrue(true);
@@ -53,17 +49,22 @@ public class IngridRealmTest extends TestCase {
 //        }
 //    }
 //
-//    public void testADMINPORTAL() throws Exception {
-//        String name = "adminportal";
-//
-//        assertNotNull(this.fIRealm.authenticate(name, name, null));
-//        Principal principal = this.fIRealm.getPrincipal(name);
-//        Collection hierarchie = this.fIRealm.getHierarchie();
-//        for (Iterator iter = hierarchie.iterator(); iter.hasNext();) {
-//            Map map = (Map) iter.next();
-//            System.out.println(map);
-//        }
-//    }
+    public void testADMINPORTAL() throws Exception {
+        String name = "TestKatAdmin";
+        BusClient client = BusClient.instance();
+        client.setBusUrl("/torwald-group:torwald-ibus");
+        ICommunication communication = StartCommunication.create(new FileInputStream("src/conf/communication.properties"));
+        communication.startup();
+        client.setCommunication(communication);
+        IngridRealm ingridRealm = new IngridRealm("IngridRealm", client.getBus(), "SHA-1");
+        assertNotNull(ingridRealm.authenticate(name, name, null));
+        Principal principal = ingridRealm.getPrincipal(name);
+        System.out.println(principal.getName());
+        assertNotNull(principal);
+        User user = (User) principal;
+        List partnerWithProvider = user.getPartnerWithProvider();
+        System.out.println(partnerWithProvider);
+    }
 //
 //    public void testMB() throws Exception {
 //        String name = "mb";

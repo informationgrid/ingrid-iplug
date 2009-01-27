a38039cba87374c9241edcb332a6f6d0dda7ab03
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-01-27 09:53:56 +0000

    fix test

--- src/java/de/ingrid/iplug/MessageHandlerCache.java
+++ src/java/de/ingrid/iplug/MessageHandlerCache.java
@@ -10,52 +10,68 @@ import org.apache.log4j.Logger;
 
 public class MessageHandlerCache implements IMessageHandler {
 
-	private final IMessageHandler _messageHandler;
-	private CacheManager _cacheManager;
-	private Cache _cache;
-	private static final Logger LOG = Logger
-			.getLogger(MessageHandlerCache.class);
+    private final IMessageHandler _messageHandler;
 
-	public MessageHandlerCache(IMessageHandler messageHandler) throws Exception {
-		_messageHandler = messageHandler;
-		_cacheManager = CacheManager.getInstance();
-		_cache = _cacheManager.getCache("ingrid-cache");
-		if (_cache == null) {
-			if (!_cacheManager.cacheExists("default")) {
-				_cache = new Cache("default", 1000, false, false, 600, 600);
-				_cacheManager.addCache(_cache);
-			} else {
-				_cache = _cacheManager.getCache("default");
-			}
-		}
-	}
+    private CacheManager _cacheManager;
 
-	@Override
-	public Message handleMessage(Message message) {
-		String cacheKey = message.toString();
-		Message ret = null;
-		if (cacheKey.indexOf("cache:on") > -1) {
-			Element element = getFromCache(cacheKey);
-			ret = element != null ? (Message) element.getValue() : null;
-		}
-		if (ret == null) {
-			ret = _messageHandler.handleMessage(message);
-			_cache.put(new Element(cacheKey, ret));
-		} 
-		return ret;
-	}
+    private Cache _cache;
 
-	private Element getFromCache(String cacheKey) {
-		Element element = null;
-		try {
-			element = _cache.get(cacheKey);
-			if (LOG.isDebugEnabled()) {
-				LOG.debug("found element in cache, with cacheKey: " + cacheKey);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return element;
-	}
+    private static final Logger LOG = Logger.getLogger(MessageHandlerCache.class);
+
+    private static final int CACHE_OFF = 0;
+
+    private static final int CACHE_ON = 1;
+
+    public MessageHandlerCache(IMessageHandler messageHandler) throws Exception {
+        _messageHandler = messageHandler;
+        _cacheManager = CacheManager.getInstance();
+        _cache = _cacheManager.getCache("ingrid-cache");
+        if (_cache == null) {
+            if (!_cacheManager.cacheExists("default")) {
+                _cache = new Cache("default", 1000, false, false, 600, 600);
+                _cacheManager.addCache(_cache);
+            } else {
+                _cache = _cacheManager.getCache("default");
+            }
+        }
+    }
+
+    @Override
+    public Message handleMessage(Message message) {
+        String cacheKey = message.toString();
+        Message ret = null;
+        int status = cacheKey.indexOf("cache: false") > -1 ? CACHE_OFF : CACHE_ON;
+        switch (status) {
+        case CACHE_OFF:
+            ret = _messageHandler.handleMessage(message);
+            _cache.put(new Element(cacheKey, ret));
+            break;
+        case CACHE_ON:
+            Element element = getFromCache(cacheKey);
+            ret = element != null ? (Message) element.getValue() : null;
+            break;
+        }
+
+        if (ret == null) {
+            // not found in cache
+            ret = _messageHandler.handleMessage(message);
+            _cache.put(new Element(cacheKey, ret));
+        }
+        
+        return ret;
+    }
+
+    private Element getFromCache(String cacheKey) {
+        Element element = null;
+        try {
+            element = _cache.get(cacheKey);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("found element in cache, with cacheKey: " + cacheKey);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return element;
+    }
 
 }
--- src/test/de/ingrid/iplug/MessageHandlerCacheTest.java
+++ src/test/de/ingrid/iplug/MessageHandlerCacheTest.java
@@ -10,38 +10,35 @@ import de.ingrid.utils.queryparser.QueryStringParser;
 
 public class MessageHandlerCacheTest extends TestCase {
 
-	public class TestHandler implements IMessageHandler {
+    public class TestHandler implements IMessageHandler {
 
-		private int _counter;
-		@Override
-		public Message handleMessage(Message arg0) {
-			_counter++;
-			return new Message();
-		}
+        private int _counter;
 
-	}
+        @Override
+        public Message handleMessage(Message arg0) {
+            _counter++;
+            return new Message();
+        }
 
-	public void testReflectMessage() throws Exception {
-		
-		TestHandler testHandler = new TestHandler();
-		MessageHandlerCache cache = new MessageHandlerCache(testHandler);
+    }
 
-		assertEquals(0, testHandler._counter);
-		for (int i = 0; i < 10; i++) {
-			IngridQuery ingridQuery = QueryStringParser.parse("foo" + i
-					+ " cache:on");
-			cache.handleMessage(new ReflectMessage("search", IPlug.class
-					.getName(), new Object[] { ingridQuery, 10, 10 }));
-		}
-		assertEquals(10, testHandler._counter);
-		
-		for (int i = 0; i < 10; i++) {
-			IngridQuery ingridQuery = QueryStringParser.parse("foo" + i
-					+ " cache:on");
-			cache.handleMessage(new ReflectMessage("search", IPlug.class
-					.getName(), new Object[] { ingridQuery, 10, 10 }));
-		}
-		assertEquals(10, testHandler._counter);
-		
-	}
+    public void testReflectMessage() throws Exception {
+
+        TestHandler testHandler = new TestHandler();
+        MessageHandlerCache cache = new MessageHandlerCache(testHandler);
+
+        assertEquals(0, testHandler._counter);
+        for (int i = 0; i < 10; i++) {
+            IngridQuery ingridQuery = QueryStringParser.parse("foo" + i + " cache: true");
+            cache.handleMessage(new ReflectMessage("search", IPlug.class.getName(), new Object[] { ingridQuery, 10, 10 }));
+        }
+        assertEquals(10, testHandler._counter);
+
+        for (int i = 0; i < 10; i++) {
+            IngridQuery ingridQuery = QueryStringParser.parse("foo" + i + " cache: true");
+            cache.handleMessage(new ReflectMessage("search", IPlug.class.getName(), new Object[] { ingridQuery, 10, 10 }));
+        }
+        assertEquals(10, testHandler._counter);
+
+    }
 }
